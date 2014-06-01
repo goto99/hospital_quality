f260a4aefb428a8cd362fd3e69ed48a84d6bc7c9
Guo Tao <goto99@gmail.com>
2014-06-01 13:11:43 +0800

    Updated comments in best.R

--- best.R
+++ best.R
@@ -7,23 +7,29 @@ best <- function(state, outcome) {
                 stop("invalid state")
         }
         
-        ## function to find the best hospital
-        compare <- function(sub, x) {
-                ## keep Hospital.Name and the corresponding outcome only.
-                sub1 <- data.frame(1:nrow(sub))
+        ## function to find the best hospital based on the given column number.
+        compare <- function(sub, x) {      
+                ## creating an empty data frame with the same rows as sub
+                sub1 <- data.frame(1:nrow(sub))  
+                
+                ## copy Hospital.Name and the given column to sub1
                 sub1$V1 <- sub$Hospital.Name
                 sub1$V2 <- sub[, x]
+                
+                ## remove nas from sub1
                 sub1 <- na.omit(sub1)
                 min = min(sub1$V2)
+                
+                ## get the subset with corresponding minimum death rate.
                 sub2 <- subset(sub1, sub1$V2 == min) 
+                
+                ## sort in alphabetical order(increasing) and return the first.
                 sort(sub2$V1,decreasing=FALSE)[1]
         }
         
-        ## keep only the columns of "Hospital.Name", "Hospital.30.Day.
-        ## Death..Mortality..Rates.from.Heart.Attack", "Hospital.30.Day.
-        ## Death..Mortality..Rates.from.Heart.Failure", or " Hospital.
-        ## 30.Day.Death..Mortality..Rates.from.Pneumonia"
-        ## Check if the outcome is one of "heart attack", "heart failure", or "pneumoni"        
+        ## Check if the outcome is one of "heart attack", "heart failure", or "pneumoni"   
+        ## and choose the corresponding column number if outcome is right, otherwise
+        ## stop the program.
         if (outcome == "heart attack") {
                 ## set the value to the column of "heart attack"
                 compare(sub, 11)
